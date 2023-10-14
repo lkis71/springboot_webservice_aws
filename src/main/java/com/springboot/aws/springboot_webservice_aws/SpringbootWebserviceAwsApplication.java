a72b62f5d16dd974d46ffd7ddaf2eece0e6a663d
lkis71 <lkis71@naver.com>
2023-10-14 16:50:44 +0900

    최초 배포

---
+++ src/main/java/com/springboot/aws/springboot_webservice_aws/SpringbootWebserviceAwsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.springboot_webservice_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/aws/springboot_webservice_aws/SpringbootWebserviceAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.springboot_webservice_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
